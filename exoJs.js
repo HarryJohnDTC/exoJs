854e61ca54c70f22b253832be1c079ccbcaef8e3
HarryJohnGeit <liva13octobre2010@gmail.com>
2024-11-09 21:51:04 +0300

    first commit

---
+++ exoJs.js
@@ -0,0 +1,56 @@
+//Nombre pair
+//Somme
+//Nombre divisible par 5
+let nombres = [1, 5, 25, 2, 4, 8, 90, 100];
+let nombresPair = [];
+let somme = 0;
+let nombresDivisiblePar5 = [];
+
+for (let i = 0; i < nombres.length; i++) {
+    somme += nombres[i];
+
+    if (nombres[i] % 2 == 0) {
+        nombresPair.push(nombres[i])
+    }
+
+    if (nombres[i] % 5 == 0) {
+        nombresDivisiblePar5.push(nombres[i])
+    }
+}
+
+console.log(
+`Somme des nombres dans le tableau nombres: ${somme}
+Nombre pair dans le tableau nombres: ${nombresPair}
+Nombre divisible par 5: ${nombresDivisiblePar5}`
+);
+
+
+//chaine de caractères +5
+//chaine de caratère misy "a"
+//remplacer "o" par "i"
+let noms = ["Andry", "Ravalomanana", "Bema", "Mom", "Koto"];
+
+let chaineSup5 = [];
+let chaineAveCa = [];
+let chaineRemplaceOi = [];
+
+for (let i = 0; i < noms.length; i++) {
+    if (noms[i].length > 5) {
+        chaineSup5.push(noms[i]);
+    }
+    if (noms[i].includes("a")) {
+        chaineAveCa.push(noms[i]);
+    }
+    if (noms[i].includes("o")) {
+        chaineRemplaceOi.push(noms[i].replace(/o/g, "i"));
+    } else {
+        chaineRemplaceOi.push(noms[i]);
+    }
+}
+
+console.log(
+    `Chaine de caractère supérieur à 5: ${chaineSup5}
+Chaine de caractère avec "a": ${chaineAveCa}
+Le tableau noms avec les noms qui contenait "o" et remplacé par "i" :
+${chaineRemplaceOi}`
+);
---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+// let persons = ["rakoto", "rasoa", 3, true];
+// console.log(persons[1]);
+// persons[2] = "Babakoto";
+// console.log(persons[2]);
+// console.log(persons);
+// persons[4]= "vaovao";
+// document.write(persons)
+
+// for (let i = 0; i < persons.length; i++) {
+//     console.log(persons[i]);  
+// }
+
+
+
+
+/*let nom = "Benja";
+nom = "Babakoto";
+console.log(nom);
+
+var prenom = "Rakoto";
+var prenom = "Bonome";
+console.log(prenom);
+
+const dateNaissance = 1988;
+console.log(dateNaissance);
+
+
+let andro = "sabotsy";
+andro = 2;
+andro = true;
+if (andro == "sabotsy") {
+    console.log("mianatra isika");
+} else {
+    console.log("Non, tsy mianatra"); 
+}
+
+console.log("12" === 12);
+console.log(12 === 12);
+console.log(!true);
+console.log(true || false);
+console.log(true && false);
+console.log(false && false);
+console.log(2<3);
+console.log(2<=3);
+console.log(2>=3);
+console.log(2 != 3);*/
+
+//+,-,/,*
+
+
+
+
+
+
+
+
+
+//
